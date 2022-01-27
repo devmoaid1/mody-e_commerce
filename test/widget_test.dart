245243a4d101baac8900413126c61f0d36d0b317
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2022-01-27 20:20:18 +0200

    added intial dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mody_ecommerce/main.dart';
+import 'package:mody_ecommerce/mody_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
